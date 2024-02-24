3e62f9cecfd03948d8d8e54ee392bf09569747f0
zakaria25101993 <8798@holbertonstudents.com>
2024-02-24 21:33:53 +0000

    ttt

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,23 +1,15 @@
-#include "main.h"
-#include <stdio.h>
-
+#include"main.h"
 /**
- * main - check the code
+ * _abs - Print absolute value of a number
+ * @n: The number to check
  *
- * Return: Always 0.
+ * Return: n if n is positiv or null
+ *        -n if n is negativ
  */
-
-int main(void)
+int _abs(int n)
 {
-    int r;
-
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
-    return (0);
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
 }
