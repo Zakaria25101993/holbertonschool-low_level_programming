67f88916ee7a0170cb1e4632301e9d32e4d869b3
zakaria25101993 <8798@holbertonstudents.com>
2024-02-29 16:25:53 +0000

    afficher le produit de deux entiers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - Checks if a character is a digit between 0 and 9.
+ * @a: The character to be checked.
+ * @b: The character to be checked.
+ * Return: produit de a * b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
