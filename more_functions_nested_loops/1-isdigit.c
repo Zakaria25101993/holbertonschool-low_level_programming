81b0d667508c1962135a6fcf5f73267bf3f7bcec
zakaria25101993 <8798@holbertonstudents.com>
2024-02-29 15:23:14 +0000

    Affiche digit

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * _isdigit - Checks if a character is a digit between 0 and 9.
  * @c: The character to be checked.
  *
  * Return: 1 if the character is uppercase.
- */
-
+ **/
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
