e4cca20ceea683fd77a1dbfd1a3ae285a1befbdc
zakaria25101993 <8798@holbertonstudents.com>
2024-02-29 14:02:16 +0000

    Afficher les majuscules

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
-int _isupper (int c)
+/**
+ * _isupper - Afficher c lorsque c'est une majuscule
+ * @c: c'est le nom de ma variable
+ * Return: always 0
+ */
+int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-        return (1);
-    else
-        return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
