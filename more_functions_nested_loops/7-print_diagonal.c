8abd3dbb5d464aee18e6b266f97808b37b2241e6
zakaria25101993 <8798@holbertonstudents.com>
2024-03-01 08:43:56 +0000

    Afficher diagonales

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * print_diagonal - Draw a diagonal line with caracter
+ * @n : number of lines ending by character
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
+
