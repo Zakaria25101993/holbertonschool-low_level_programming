583e17a8b2144f7916e7f1715e8d18a454d2d35a
zakaria25101993 <8798@holbertonstudents.com>
2024-03-01 08:47:58 +0000

    print #

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_square - Draw a square of # caracters
+ * @size : number of # per line
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{	int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
