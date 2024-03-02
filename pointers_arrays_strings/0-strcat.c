af7d6e999c2d55ec3e5372888b2b61b5e8a4c66c
zakaria25101993 <8798@holbertonstudents.com>
2024-03-02 17:19:45 +0000

    go

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,22 +1,21 @@
-#include "holberton.h"
+#include"main.h"
 /**
- * _strcat -  function that concatenates two strings.
- * @src: parameter to a src
- * @dest: parameter to append
- * Return: Always 0.
+ * _strcat - concatenate two strings
+ * @dest: string to be concatened
+ * @src: string to add
  */
 char *_strcat(char *dest, char *src)
 {
-	int a = 0;
-	int b = 0;
+	int i = 0, j = 0;
 
-	while (dest[a] != '\0')
-		a++;
-	while (src[b] != '\0')
+	while (dest[i] != '\0')
 	{
-		dest[a] = src[b];
-		b++;
-		a++;
+		i++;
 	}
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+
+	dest[i + j] = '\0';
+
 	return (dest);
 }
