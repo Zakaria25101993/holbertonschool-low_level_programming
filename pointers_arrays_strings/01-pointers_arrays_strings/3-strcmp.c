c09975bf3335f5e2e14bee4def698eaa0bca1a23
zakaria25101993 <8798@holbertonstudents.com>
2024-03-21 16:50:10 +0000

    gogogogo

--- pointers_arrays_strings/01-pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/01-pointers_arrays_strings/3-strcmp.c
@@ -7,15 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0;
+int i = 0;
 
-	while (s1[a] != '\0')
+while (s1[i] != '\0' && s1[i] == s2[i])
 	{
-		if (s1[a] < s2[a])
-			return (s1[a] - s2[a]);
-		if (s1[a] > s2[a])
-			return (s1[a] - s2[a]);
-		a++;
+	i++;
 	}
-	return (0);
+
+	return (s1[i] - s2[i]);
 }
