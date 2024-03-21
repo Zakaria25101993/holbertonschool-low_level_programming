e6ce33b5464ac0c27f1f231c8a89ce78f36b32f0
zakaria25101993 <8798@holbertonstudents.com>
2024-03-21 17:03:49 +0000

    gogogo

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,33 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "variadic_functions.h"
 
 /**
- * print_strings - prints strings
- * @separator: separator between strings
- * @n: number of arguments
- */
-void print_strings(const char *separator, const unsigned int n, ...)
+  * print_numbers - ...
+  * @separator: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
-	char *str;
+	va_list args;
+	unsigned int i = 0;
 
-	va_list list;
-
-	va_start(list, n);
-
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		str = va_arg(list, char *);
-		if (!str)
-			str = "(nil)";
-		if (!separator)
-			printf("%s", str);
-		else if (separator && i == 0)
-			printf("%s", str);
-		else
-			printf("%s%s", separator, str);
+		va_start(args, n);
+
+		while (i < n)
+		{
+			printf("%d", va_arg(args, int));
+
+			if (i != n - 1  && separator != NULL)
+				printf("%s", separator);
+
+			i++;
+		}
+		va_end(args);
 	}
-
 	printf("\n");
-
-	va_end(list);
 }
