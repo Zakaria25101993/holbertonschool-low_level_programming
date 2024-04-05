aaa409a3620814c5b5ebcfe4ba6678df00f16882
zakaria25101993 <8798@holbertonstudents.com>
2024-04-05 14:07:58 +0000

    gogggogo

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: number to transform to binary
+ * Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
